12b683c3ac8184657bd52804da233920facf0d4e
nicolasdantas <nicolas.dantas@gmail.com>
2020-11-20 06:17:22 +0100

    footer is working

--- src/components/OrderConfirmed.js
+++ src/components/OrderConfirmed.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import moment from 'moment'
+import moment from 'moment';
+import Footer from '../components/Footer';
 import { ProducerContext } from '../context/ProducerContext';
 import '../styles/ProducerDetail.scss';
 
@@ -9,17 +10,22 @@ const OrderConfirmed = (props) => {
   const producerId = parseInt(props.match.params.producer);
 
   return (
-    producers.length !== 0 &&
-    <div className='order-confirmed'>
-      <h1>Thanks guy for your order !</h1>
-      <h2>
-        {
-          producers.filter((producer) => producer.id === producerId)[0]
-            .meetMessage
-        }{' '}at {moment().add(2, 'hours').format('hh:mm')} ...
-      </h2>
-      <h2>Don't be in late and don't tell anyone ...</h2>
-    </div>
+    producers.length !== 0 && (
+      <>
+        <div className='order-confirmed'>
+          <h1>Thanks guy for your order !</h1>
+          <h2>
+            {
+              producers.filter((producer) => producer.id === producerId)[0]
+                .meetMessage
+            }{' '}
+            at {moment().add(2, 'hours').format('hh:mm')} ...
+          </h2>
+          <h2>Don't be in late and don't tell anyone ...</h2>
+        </div>
+        <Footer />
+      </>
+    )
   );
 };
 
