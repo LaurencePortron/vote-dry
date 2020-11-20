1af656090c28cc972c167c5baf6e34f6501cc27b
AntoineGGG <a.gorenflot@gmx.fr>
2020-11-20 06:51:28 +0100

    fix consumerpage problem

--- src/components/ConsumerPage.js
+++ src/components/ConsumerPage.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import './ConsumerPage.css';
-import ProducerCard from './ProducerCard';
+import ProducerCardTest from './ProducerCardTest';
 import Filter from './Filter';
 import SearchBar from './SearchBar';
 import { ProducerContext } from '../context/ProducerContext';
 import NavBarConsumer from './NavBarConsumer';
+import './ConsumerPage.css';
 
 export default function ConsumerPage(props) {
   const { producers, alcoholSearched } = useContext(ProducerContext);
@@ -12,21 +12,21 @@ export default function ConsumerPage(props) {
   const displayProducers = () => {
     return alcoholSearched.length > 0
       ? alcoholSearched.map((producer) => {
-          return <ProducerCard key={producer.id} producer={producer} />;
+          return <ProducerCardTest key={producer.id} producer={producer} />;
         })
       : producers.map((producer) => {
-          return <ProducerCard key={producer.id} producer={producer} />;
+          return <ProducerCardTest key={producer.id} producer={producer} />;
         });
   };
 
   return (
     <div>
       <NavBarConsumer />
-      <SearchBar />
-      <div>
-        <Filter />
+      <div className='searchbar-container'>
+        <SearchBar />
       </div>
-      {displayProducers()}
+
+      <div className='container-map'>{displayProducers()}</div>
     </div>
   );
 }
